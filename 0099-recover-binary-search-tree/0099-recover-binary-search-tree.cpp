1ad868d8c8201e5e882b70c1c9a79bc8e6ce5bd0
kandika-Rohan <rohankandika05@gmail.com>
2024-07-11 15:03:41 +0530

    Time: 12 ms (92.87%), Space: 60.9 MB (10.99%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -10,31 +10,43 @@
  * };
  */
 class Solution {
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+
 public:
-    void inorder(TreeNode*root,vector<int>&ds){
-        if(!root){
+    void inorder(TreeNode* root) {
+        if (!root) {
             return;
         }
-        inorder(root->left,ds);
-        ds.push_back(root->val);
-        inorder(root->right,ds);
-    }
-    void solve(TreeNode*root,vector<int>&ds,int &i){
-        if(!root){
-            return ;
+        inorder(root->left);
+        
+        if (prev != nullptr && prev->val > root->val) {
+            if (first == nullptr) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
         }
-        solve(root->left,ds,i);
-        if(ds[i] != root->val){
-            root->val=ds[i];
-        }
-        i++;
-        solve(root->right,ds,i);
+        prev = root;
+        inorder(root->right);
     }
+
     void recoverTree(TreeNode* root) {
-        vector<int>ds;
-        inorder(root,ds);
-        sort(ds.begin(),ds.end());
-        int i=0;
-        solve(root,ds,i);
+        first = middle = last = nullptr;
+        prev = new TreeNode(INT_MIN);
+        
+        // Perform the in-order traversal to find the two nodes
+        inorder(root);
+        
+        // Fix the swapped nodes
+        if (first && last) {
+            swap(first->val, last->val);
+        } else if (first && middle) {
+            swap(first->val, middle->val);
+        }
     }
 };
