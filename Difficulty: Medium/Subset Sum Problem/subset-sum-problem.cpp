25fa074be64fd44009b0fbc1ccb1dc80cbb9d190
kandika-Rohan <rohankandika05@gmail.com>
2024-07-15 00:08:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool f(int ind,int sum,vector<int>arr,vector<vector<int>>&dp){
+        if(sum==0){
+            return true;
+        }
+        if(ind==0){
+            return arr[0]==sum;
+        }
+        if(dp[ind][sum] != -1){
+            return dp[ind][sum];
+        }
+        bool nottake=f(ind-1,sum,arr,dp);
+        bool take=false;
+        if(arr[ind]<=sum){
+            take=f(ind-1,sum-arr[ind],arr,dp);
+        }
+        return dp[ind][sum]=take || nottake;
+    }
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return f(n-1,sum,arr,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
