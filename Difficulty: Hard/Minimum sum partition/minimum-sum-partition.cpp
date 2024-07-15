5b9fe6c3eac2ee898104f5558eb16a6e8673097a
kandika-Rohan <rohankandika05@gmail.com>
2024-07-15 10:38:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+#include<bits/stdc++.h>
+class Solution{
+
+  public:
+	int minDifference(int nums[], int n)  { 
+	     int totSum = 0;
+        for (int i=0; i<n; i++) totSum += nums[i];
+
+        int k = totSum;
+
+        // Step 1: Create a 2D array of size n * (totSum + 1)
+        vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
+
+        // Base cases
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        if (nums[0] <= k) dp[0][nums[0]] = true;
+
+        // Do tabulation
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                bool notTake = dp[ind - 1][target];
+                bool take = false;
+                if (nums[ind] <= target) take = dp[ind - 1][target - nums[ind]];
+                dp[ind][target] = take || notTake;
+            }
+        }
+
+        // Find the partition with min sum difference
+        int mini = 1e9;
+        for (int s1 = 0; s1 <= totSum / 2; s1++) {
+            if (dp[n - 1][s1]) {
+                mini = min(mini, abs((totSum - s1) - s1));
+            }
+        }
+
+        return mini;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
