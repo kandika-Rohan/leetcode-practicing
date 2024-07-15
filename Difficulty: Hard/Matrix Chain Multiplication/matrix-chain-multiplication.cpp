c3cfb7a4f53bf0b46c1e96b111c98fa83f04f378
kandika-Rohan <rohankandika05@gmail.com>
2024-07-15 11:49:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -28,10 +28,25 @@ public:
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>>dp(N,vector<int>(N,-1));
+        int dp[N][N];
+        for(int i=1;i<N;i++){
+            dp[i][i]=0;
+        }
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int mini=1e9;
         
-        
-        return f(1,N-1,arr,dp);
+                for(int k=i;k<j;k++){
+                     int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                     if(steps<mini){
+                         mini=steps;
+                     }
+           
+             }
+         dp[i][j]=mini;
+            }
+        }
+        return dp[1][N-1];
     }
 };
 
