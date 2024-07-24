70ce38e15c55149c802c85c85041f49861fa2c34
kandika-Rohan <rohankandika05@gmail.com>
2024-07-24 22:55:56 +0530

    Time: 333 ms (16.54%), Space: 16.1 MB (72.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int totalsum=accumulate(nums.begin(),nums.end(),0);
-        //base case total sum is odd it is not possible to divide the array into tow equal sums
 
-        if(totalsum%2){
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+
+        int n=nums.size(); 
+
+        if(totalsum <0 || (totalsum%2 != 0)){
             return false;
         }
-        int curr_sum=totalsum/2;
-        int n=nums.size();
+        int sum=totalsum/2;
 
-        vector<vector<int>>dp(n,vector<int>(curr_sum+1,0));
+        vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
 
         for(int i=0;i<n;i++){
-            dp[i][0]=true;
+            dp[i][0]=true; // if the totalsum is zero 
         }
-
-        if(nums[0]<=curr_sum){
+        if(nums[0] <= sum){
             dp[0][nums[0]]=true;
         }
 
         for(int i=1;i<n;i++){
-            for(int target=0;target<=curr_sum;target++){
+            for(int target=1;target<=sum;target++){
                 bool nottake=dp[i-1][target];
                 bool take=false;
-                if(target>=nums[i]){
+                if(nums[i]<=target){
                     take=dp[i-1][target-nums[i]];
                 }
                 dp[i][target]=take || nottake;
             }
         }
-        return dp[n-1][curr_sum];
+        return dp[n-1][sum];
     }
 };
