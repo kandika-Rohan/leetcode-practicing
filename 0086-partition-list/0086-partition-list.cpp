e5e8f35ded911edbdec873c661c5efcd2e5cbbc7
kandika-Rohan <rohankandika05@gmail.com>
2024-07-24 16:29:43 +0530

    Time: 0 ms (100%), Space: 14.8 MB (14.61%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -1,40 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(!head)return nullptr;
-        vector<int>list1;
-        vector<int>list2;
-        ListNode*temp=head;
-        while(temp){
-            if(temp->val < x){
-                list1.push_back(temp->val);
-            }
-            if(temp->val >= x){
-                list2.push_back(temp->val);
-            }
-            temp=temp->next;
-        }
-        for(auto it:list2){
-            list1.push_back(it);
+        if (!head) {
+            return head;
         }
 
-        ListNode*root=new ListNode(list1[0]);
-        ListNode*tmp=root;
-        for(int i=1;i<list1.size();i++){
-            ListNode*ptr=new ListNode(list1[i]);
-            tmp->next=ptr;
-            tmp=ptr;
+        ListNode* lessHead = new ListNode(0);
+        ListNode* greaterHead = new ListNode(0);
+
+        ListNode* less = lessHead;
+        ListNode* greater = greaterHead;
+
+        ListNode* temp = head;
+        while (temp) {
+            if (temp->val < x) {
+                less->next = temp;
+                less = temp;
+            } else {
+                greater->next = temp;
+                greater = temp;
+            }
+            temp = temp->next;
         }
-        return root;
+
+        greater->next = nullptr; // Terminate the greater list correctly
+        less->next = greaterHead->next; // Correctly link the end of the less list to the start of the greater list
+
+        ListNode* newHead = lessHead->next; // Correctly set the new head to the next of lessHead
+
+        // Clean up dummy nodes to avoid memory leaks
+        delete lessHead;
+        delete greaterHead;
+
+        return newHead; // Return the correct new head
     }
 };
