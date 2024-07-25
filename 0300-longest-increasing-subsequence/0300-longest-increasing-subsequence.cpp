94f47ff372777da3cc18062b526710054fe4809b
kandika-Rohan <rohankandika05@gmail.com>
2024-07-25 18:13:07 +0530

    Time: 230 ms (37.49%), Space: 294.9 MB (7.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,37 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int lengthOfLIS(std::vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(0, -1, nums, nums.size(),dp);
+    // int f(int ind,int prev_ind,vector<int>&nums,int n,vector<vector<int>>&dp){
+    //     if(ind == n){
+    //         return 0;
+    //     }
+    //     if(dp[ind][prev_ind+1] != -1){
+    //         return dp[ind][prev_ind+1];
+    //     }
+    //     int nottake=f(ind+1,prev_ind,nums,n,dp);
+    //     int take=0;
+    //     if(prev_ind == -1 || nums[prev_ind] < nums[ind]){
+    //         take=1+f(ind+1,ind,nums,n,dp);
+    //     }
+    //     return dp[ind][prev_ind+1]=max(take,nottake);
+    // }
+  int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return 0;
+
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    for (int i = n - 1; i >= 0; --i) {
+        for (int prev = i - 1; prev >= -1; --prev) {
+            int notTake = dp[i + 1][prev + 1];
+            int take = 0;
+            if (prev == -1 || nums[i] > nums[prev]) {
+                take = 1 + dp[i + 1][i + 1];
+            }
+            dp[i][prev + 1] = max(take, notTake);
+        }
     }
 
-private:
-    int f(int ind, int prev_ind, std::vector<int>& nums, int n,vector<vector<int>>&dp) {
-        if (ind == n) {
-            return 0;
-        }
-        if(dp[ind][prev_ind+1] != -1){
-            return dp[ind][prev_ind+1];
-        }
-        int len = 0 + f(ind + 1, prev_ind, nums, n,dp);
-        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) {
-            len = std::max(len, 1 + f(ind + 1, ind, nums, n,dp));
-        }
-        return dp[ind][prev_ind+1]=len;
-    }
+    return dp[0][0];
+}
+
 };
