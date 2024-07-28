612555d5156abe4d66f78ae3926ef92416073ea6
kandika-Rohan <rohankandika05@gmail.com>
2024-07-28 14:10:50 +0530

    Time: 253 ms (5.01%), Space: 146.7 MB (5.46%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool check(vector<int> quantities, int mid, int store, int n){
+        int s = 0;
+        for(int i = 0; i < n; i++){
+            int quan = quantities[i];
+            //distributing predecting quantities mid to store s, incresing s++
+            while(quan > 0){
+                if(quan >= mid){
+                    quan -= mid;
+                    s++;
+                }
+                else{
+                    s++;
+                    quan = 0;
+                }
+                // when more store required than given store
+                if(s > store) return false;
+            }
+        }
+        return true;
+    }
+    int minimizedMaximum(int store, vector<int>& quantities) {
+        int n = quantities.size();
+        int right;
+        for(int i = 0; i < n; i++){
+            right = max(right, quantities[i]);
+        }
+        int res = -1;
+        int left = 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(check(quantities, mid , store, n)){
+                res = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+};
