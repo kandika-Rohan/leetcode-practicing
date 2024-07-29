f2558aeab026464d623bc2e089fc3bafb4f6eb3e
kandika-Rohan <rohankandika05@gmail.com>
2024-07-29 18:27:12 +0530

    Time: 4 ms (84.12%), Space: 17.2 MB (25.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        int startColor = image[sr][sc];
-
-        if (startColor == color) {
+        int c=image[sr][sc];
+        if(c == color){
             return image;
         }
-        vector<int> delRow = {-1, 0, 1, 0};
-        vector<int> delCol = {0, 1, 0, -1};
 
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        image[sr][sc] = color;
+        vector<vector<int>>vis=image;
+        int n=image.size();
+        int m=image[0].size();
 
-        while (!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+
+        vis[sr][sc]=color;
+
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            auto node=q.front();
             q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int newRow = row + delRow[i];
-                int newCol = col + delCol[i];
-
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && image[newRow][newCol] == startColor) {
-                    image[newRow][newCol] = color;
-                    q.push({newRow, newCol});
+            int row=node.first;
+            int col=node.second;
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m &&
+                image[nrow][ncol]==c && vis[nrow][ncol] != color ){
+                    vis[nrow][ncol]=color;
+                    q.push({nrow,ncol});
                 }
             }
         }
+        return vis;
 
-        return image;
     }
 };
