a4f514b0f6ce52dc184fa18e5b93cb4c5452d13f
kandika-Rohan <rohankandika05@gmail.com>
2024-08-03 00:57:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,32 +1,32 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
 
-class Solution 
-{
-    public:
-    //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M, int n) 
-    {
-        // code here 
-        vector<int>iknow(n,0);
-        vector<int>knowme(n,0);
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        int n=mat.size();
+        vector<int>know_me(n,0);
+        vector<int>i_know_me(n,0);
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(M[i][j] == 1){
-                    iknow[i]++;
-                    knowme[j]++;
+                if(mat[i][j]==1){
+                    know_me[j]++;
+                    i_know_me[i]++;
                 }
             }
         }
         
         for(int i=0;i<n;i++){
-            if(iknow[i] == 0 && knowme[i] == n-1){
+            if(i_know_me[i]==0 && know_me[i]==n-1){
                 return i;
             }
         }
@@ -36,25 +36,20 @@ class Solution
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        vector<vector<int> > M( n , vector<int> (n, 0));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                cin>>M[i][j];
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
             }
         }
         Solution ob;
-        cout<<ob.celebrity(M,n)<<endl;
-
+        cout << ob.celebrity(M) << endl;
     }
 }
 
