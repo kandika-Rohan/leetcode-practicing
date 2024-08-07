3b26418459918353ff500fd068beab59ea01b61f
kandika-Rohan <rohankandika05@gmail.com>
2024-08-07 18:09:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,56 +8,32 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-  
-    void dfs(int node,vector<int>&vis,stack<int>&st,vector<pair<int,int>>adj[]){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            int v=it.first;
-            if(!vis[v]){
-                dfs(v,vis,st,adj);
-            }
-        }
-        st.push(node);
-    }
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
         // code here
-        vector<pair<int,int>>adj[N];
-        
-        for(int i=0;i<M;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int wt=edges[i][2];
-            adj[u].push_back({v,wt});
-        }
-        vector<int>vis(N,0);
-        stack<int>st;
-        for(int i=0;i<N;i++){
-            if(!vis[i]){
-                dfs(i,vis,st,adj);
-            }
-        }
-        
-        vector<int>dis(N,1e9);
-        
-        dis[0]=0;
-        while(!st.empty()){
-            auto node=st.top();
-            st.pop();
-            
-            for(auto it:adj[node]){
-                int v=it.first;
-                int wt=it.second;
-                if(wt+dis[node]<dis[v]){
-                    dis[v]=wt+dis[node];
+        vector<int>distance(N,1e9);
+        distance[0]=0;
+        for(int i=0;i<N-1;i++){
+            for(auto it:edges){
+                int u=it[0];
+                int v=it[1];
+                int wt=it[2];
+                if(distance[u]!= 1e9 && wt+distance[u]<distance[v]){
+                    distance[v]=wt+distance[u];
                 }
             }
         }
-        for(int i=0;i<N;i++){
-            if(dis[i]==1e9){
-                dis[i]=-1;
+        for(auto it:edges){
+                int u=it[0];
+                int v=it[1];
+                int wt=it[2];
+                if(distance[u]!= 1e9 && wt+distance[u]<distance[v]){
+                    return {-1};
+                }
             }
-        }
-        return dis;
+            for(int i=0;i<N;i++){
+                if(distance[i]==1e9)distance[i]=-1;
+            }
+        return distance;
     }
 };
 
