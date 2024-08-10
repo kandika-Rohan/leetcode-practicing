5297ac0afae791d5e2573389c44fe96ad93639aa
kandika-Rohan <rohankandika05@gmail.com>
2024-08-10 22:05:57 +0530

    Time: 0 ms (100%), Space: 7.5 MB (6.9%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f(int n,vector<int>&dp){
+        if(n<=1)return 1;
+        if(dp[n] != -1)return dp[n];
+
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+=f(i-1,dp)*f(n-i,dp);
+        }
+        dp[n]=ans;
+        return dp[n];
+    }
+    int numTrees(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
+};
