852e077263baa2554727d5993ac8aac94da9f3d2
kandika-Rohan <rohankandika05@gmail.com>
2024-08-13 19:11:57 +0530

    Time: 4 ms (57.95%), Space: 9.4 MB (15.03%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>&nums){
+    int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
         if(i>j)return 0;
 
-        int op1=nums[i]+min(f(i+2,j,nums),f(i+1,j-1,nums));
+        if(dp[i][j] != -1)return dp[i][j];
+        int op1=nums[i]+min(f(i+2,j,nums,dp),f(i+1,j-1,nums,dp));
 
         
-        int op2=nums[j]+min(f(i+1,j-1,nums),f(i,j-2,nums));
+        int op2=nums[j]+min(f(i+1,j-1,nums,dp),f(i,j-2,nums,dp));
 
 
-        return max(op1,op2);
+        return dp[i][j]=max(op1,op2);
     }
     bool predictTheWinner(vector<int>& nums) {
         
         int n=nums.size();
         int total=accumulate(nums.begin(),nums.end(),0);
-
-        int val=f(0,n-1,nums);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int val=f(0,n-1,nums,dp);
         cout<<val<<endl;
-
         int diff=total-val;
 
         return diff>val?false:true;
