472c8ac50e7262d25b5b6819e372dbded84c680d
kandika-Rohan <rohankandika05@gmail.com>
2024-08-13 23:10:55 +0530

    Time: 22 ms (62.53%), Space: 26 MB (24.06%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+
     string shortestCommonSupersequence(string str1, string str2) {
+        
+
         int n=str1.size();
         int m=str2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]){
+                if(str1[i-1] == str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -16,11 +20,11 @@ public:
             }
         }
 
+        string ans="";
         int i=n;
         int j=m;
-        string ans="";
         while(i>0 && j>0){
-            if(str1[i-1]==str2[j-1]){
+            if(str1[i-1] == str2[j-1]){
                 ans+=str1[i-1];
                 i--;
                 j--;
@@ -35,15 +39,14 @@ public:
             }
         }
         while(i>0){
-            ans+=str1[i-1];i--;
+            ans+=str1[i-1];
+            i--;
         }
         while(j>0){
             ans+=str2[j-1];
             j--;
         }
-
         reverse(ans.begin(),ans.end());
         return ans;
-
     }
 };
