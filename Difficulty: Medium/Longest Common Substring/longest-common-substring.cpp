8221d81223685912eea1adcfa26d8ac58c31f571
kandika-Rohan <rohankandika05@gmail.com>
2024-08-14 00:37:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -7,18 +7,17 @@ using namespace std;
 class Solution {
   public:
     int longestCommonSubstr(string str1, string str2) {
-        // your code here
+      
         int n=str1.size();
         int m=str2.size();
         
-        // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+     
         int maxi=0;
         vector<int>curr(m+1,0);
         vector<int>prev(m+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(str1[i-1] == str2[j-1]){
-
                     curr[j]=1+prev[j-1];
                 }
                 else{
