cdef7b596cbffb9bc3e8d0b12659266294986ccd
kandika-Rohan <rohankandika05@gmail.com>
2024-08-18 08:10:44 +0530

    Time: 32 ms (5.03%), Space: 26 MB (6.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    vector<int> leftmaxi(vector<int>& arr, int n) {
-        vector<int> ans(n, 0); // Change initialization to 0 instead of -1
-        int maxLeft = 0; // Initialize maxLeft to 0
-        for (int i = 0; i < n; i++) {
-            ans[i] = maxLeft; // Assign the maxLeft value to ans
-            maxLeft = max(maxLeft, arr[i]); // Update maxLeft to the maximum value seen so far
+    vector<int>leftmax(vector<int>&arr,int n){
+        vector<int>ans(n,0);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            ans[i]=maxi;
+            maxi=max(maxi,arr[i]);
         }
+
         return ans;
     }
 
-    vector<int> rightmaxi(vector<int>& arr, int n) {
-        vector<int> ans(n, 0); // Change initialization to 0 instead of -1
-        int maxRight = 0; // Initialize maxRight to 0
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] = maxRight; // Assign the maxRight value to ans
-            maxRight = max(maxRight, arr[i]); // Update maxRight to the maximum value seen so far
+    vector<int>rightmax(vector<int>&arr,int n){
+        vector<int>ans(n,0);
+        int maxi=0;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=maxi;
+            maxi=max(maxi,arr[i]);
         }
         return ans;
     }
-
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> left = leftmaxi(height, n);
-        vector<int> right = rightmaxi(height, n);
-        int waterTrapped = 0;
-        for (int i = 0; i < n; i++) {
-            int minHeight = min(left[i], right[i]); // Find the minimum of left and right heights
-            if (minHeight > height[i]) {
-                waterTrapped += minHeight - height[i]; // Calculate the trapped water
+        int maxi=0;
+        int n=height.size();
+
+        vector<int>left=leftmax(height,n);
+        vector<int>right=rightmax(height,n);
+
+        for(int i=0;i<n;i++){
+            int val=min(left[i],right[i]);
+            cout<<val<<" ";
+            if(val>height[i]){
+                val-=height[i];
+                maxi+=val;
             }
+    
         }
-        return waterTrapped;
+
+        return maxi;
     }
 };
