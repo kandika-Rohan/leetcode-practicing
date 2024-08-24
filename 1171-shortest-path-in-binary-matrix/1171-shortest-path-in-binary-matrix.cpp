bbf280e6ccd0f7142cc5cd61cc9faca51c99105d
kandika-Rohan <rohankandika05@gmail.com>
2024-08-24 22:35:36 +0530

    Time: 36 ms (93.67%), Space: 25.4 MB (31.79%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(grid[0][0] ==1 || grid[n-1][n-1] == 1)return -1;
+        
 
+        int n=grid.size();
+        int m=grid[0].size();
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)return -1;
+        vector<vector<int>>vis(n,vector<int>(m,0));
 
         queue<pair<int,pair<int,int>>>q;
-
         q.push({1,{0,0}});
-
-        grid[0][0]=1;
+        vis[0][0]=1;
         while(!q.empty()){
             auto val=q.front();
             q.pop();
             int dist=val.first;
             int row=val.second.first;
             int col=val.second.second;
-
-            if(row == n-1 && col == n-1)return dist;
+            if(row == n-1 && col == m-1){
+                return dist;
+            }
 
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nrow=row+i;
                     int ncol=col+j;
-                    if(nrow >=0 && nrow <n && ncol>=0 && 
-                    ncol <n && grid[nrow][ncol] == 0){
+                    if(nrow>=0 && nrow < n && ncol>=0 && ncol < m && 
+                    grid[nrow][ncol]==0 && !vis[nrow][ncol]){
                         q.push({dist+1,{nrow,ncol}});
-                        grid[nrow][ncol]=1;
+                        vis[nrow][ncol]=1;
                     }
                 }
             }
         }
 
         return -1;
-
     }
 };
