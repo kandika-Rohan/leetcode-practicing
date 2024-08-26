f20e7372edacf34d6ad692800be03d493639aad4
kandika-Rohan <rohankandika05@gmail.com>
2024-08-26 19:33:33 +0530

    Time: 3 ms (25.52%), Space: 7.5 MB (44.6%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.cpp
+++ 2507-number-of-common-factors/2507-number-of-common-factors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int commonFactors(int a, int b) {
         int cnt=0;
-        for(int i=1;i<=max(a,b);i++){
+        for(int i=1;i<=min(a,b);i++){
             if(a%i == 0 && b%i == 0){
                 cnt++;
             }
