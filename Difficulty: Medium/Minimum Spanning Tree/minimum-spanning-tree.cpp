9844e212034e065625f5768083b221dd7e4f597f
kandika-Rohan <rohankandika05@gmail.com>
2024-09-05 14:23:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,43 +3,103 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class Dsu{
+    private:
+       vector<int>parent,size,rank;
+    public:
+       Dsu(int n){
+           parent.resize(n+1);
+           size.resize(n+1,1);
+           rank.resize(n+1,0);
+           for(int i=0;i<=n;i++){
+               parent[i]=i;
+               
+           }
+       }
+       int find(int x){
+           if(parent[x] == x)return x;
+           return parent[x]=find(parent[x]);
+       }
+       
+       void unite(int x,int y){
+           int u=find(x);
+           int v=find(y);
+           if(u == v) return;
+           
+           if(size[u]<size[v]){
+               size[v]+=size[u];
+               parent[u]=v;
+               
+           }
+           else if(size[u]>size[v]){
+               size[u]+=size[v];
+               parent[v]=u;
+           }
+           else{
+               size[u]+=size[v];
+               parent[v]=u;
+           }
+       }
+       void uniteByRank(int x,int y){
+           
+           int u=find(x);
+           int v=find(y);
+           if(u == v) return;
+           
+           if(rank[u]<rank[v]){
+               parent[u]=v;
+           }
+           else if(rank[u]>rank[v]){
+               parent[v]=u;
+           }
+           else{
+               parent[v]=u;
+               rank[u]++;
+           }
+       }
+       
+};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        
-        vector<int>vis(V,0);
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,
-        greater<pair<int,int>>>pq;
-        
-        
-        pq.push({0,0});
-        int sum=0;
-        
-        while(!pq.empty()){
-            auto val=pq.top();
-            pq.pop();
-            int weight=val.first;
-            int node=val.second;
-            if(vis[node])continue;
-            sum+=weight;
-            vis[node]=1;
-            
-            for(auto it:adj[node]){
-                int adjnode=it[0];
+        // code here
+        vector<pair<int,pair<int,int>>>adjlist;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int u=i;
+                int v=it[0];
                 int wt=it[1];
-                if(!vis[adjnode]){
-                    pq.push({wt,adjnode});
-                }
+                adjlist.push_back({wt,{u,v}});
+                
             }
         }
         
-        return sum;
+        sort(adjlist.begin(),adjlist.end());
+        Dsu ds(V);
+        
+        int ans=0;
+        
+      for(auto it:adjlist){
+          int wt=it.first;
+          int u=it.second.first;
+          int v=it.second.second;
+          if(ds.find(u) == ds.find(v)){
+                    continue;
+                }
+                else{
+                    ans+=wt;
+                    ds.unite(u,v);
+                }
+      }
+        
+        return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
