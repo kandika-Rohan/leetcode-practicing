bcc96d9152cfc2bb535dbdd649ee7c1299a34ab7
kandika-Rohan <rohankandika05@gmail.com>
2024-09-06 23:12:34 +0530

    Time: 102 ms (17.96%), Space: 51 MB (88.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        
         int maxi=0;
         for(int i=0;i<nums.size();i++){
             if(maxi<i)return false;
             maxi=max(maxi,nums[i]+i);
-            if(maxi > nums.size()-1)return true;
+            cout<<maxi<<endl;
+            if(maxi>=nums.size()-1)return true;
+            
         }
-        return true;
+        return false;
     }
 };
