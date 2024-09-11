77facc1fc561f94cd739551c90ca922c0658b74f
kandika-Rohan <rohankandika05@gmail.com>
2024-09-11 18:34:39 +0530

    Time: 47 ms (77.29%), Space: 34.7 MB (51.77%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,45 +1,45 @@
 class Solution {
-public:
-    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>&mat,int delrow[],int delcol[],int n,int m){
-        vis[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow < n && ncol >=0 && ncol <m && mat[nrow][ncol] == 1
-            && !vis[nrow][ncol]){
-                dfs(nrow,ncol,vis,mat,delrow,delcol,n,m);
+public: 
+    int delrow[4]={-1,0,1,0};
+    int delcol[4]={0,1,0,-1};
+    void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<int>>&grid){
+        vis[i][j]=1;
+        for(int k=0;k<4;k++){
+            int nrow=i+delrow[k];
+            int ncol=j+delcol[k];
+            if(nrow >=0 && nrow < grid.size()
+            && ncol >=0 && ncol <grid[0].size() && 
+            grid[nrow][ncol] ==1 && !vis[nrow][ncol]){
+                dfs(nrow,ncol,vis,grid);
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>mat=grid;
         vector<vector<int>>vis(n,vector<int>(m,0));
-
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
         for(int i=0;i<m;i++){
-            if(!vis[0][i] && mat[0][i] == 1){
-                dfs(0,i,vis,mat,delrow,delcol,n,m);
+            if(grid[0][i] == 1 && !vis[0][i]){
+                dfs(0,i,vis,grid);
             }
-            if(!vis[n-1][i] && mat[n-1][i] == 1){
-                dfs(n-1,i,vis,mat,delrow,delcol,n,m);
+            if(grid[n-1][i] == 1 && !vis[n-1][i]){
+                dfs(n-1,i,vis,grid);
             }
         }
 
         for(int i=0;i<n;i++){
-            if(!vis[i][0] && mat[i][0] == 1){
-                dfs(i,0,vis,mat,delrow,delcol,n,m);
+            if(grid[i][0] == 1 && !vis[i][0]){
+                dfs(i,0,vis,grid);
             }
-            if(!vis[i][m-1] && mat[i][m-1] == 1){
-                dfs(i,m-1,vis,mat,delrow,delcol,n,m);
+            if(grid[i][m-1] == 1 && !vis[i][m-1]){
+                dfs(i,m-1,vis,grid);
             }
         }
+
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j] ==1 && !vis[i][j]){
+                if(!vis[i][j] && grid[i][j] ==1){
                     cnt++;
                 }
             }
