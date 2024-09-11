605c9b74533da5db8f6424aa0e249f0eb23b920f
kandika-Rohan <rohankandika05@gmail.com>
2024-09-11 17:18:38 +0530

    Time: 60 ms (29.44%), Space: 36.7 MB (66.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,37 +3,37 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>ans(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
+                if(mat[i][j] == 0){
+                    dist[i][j]=0;
+                    q.push({i,j});
                     vis[i][j]=1;
-                    q.push({{i,j},0});
                 }
             }
         }
-        int delrow[]={-1,0,+1,0};
-        int delcol[]={0,1,0,-1};
-      
-        while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int dis=q.front().second;
-            q.pop();
-            ans[row][col]=dis;
-            for(int i=0;i<4;i++){
 
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int row=it.first;
+            int col=it.second;
+            for(int i=0;i<4;i++){
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol <m
-                && mat[nrow][ncol]==1 && !vis[nrow][ncol]){
+                if(nrow>=0 && nrow<n && ncol >=0 && ncol < m 
+                &&mat[nrow][ncol] ==1 && !vis[nrow][ncol]){
+                    dist[nrow][ncol]=dist[row][col]+1;
                     vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},dis+1});
+                    q.push({nrow,ncol});
                 }
             }
         }
-        return ans;
+        return dist;
     }
 };
