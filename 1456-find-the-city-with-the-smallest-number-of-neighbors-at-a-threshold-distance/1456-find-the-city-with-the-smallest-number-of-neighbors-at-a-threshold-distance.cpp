b5cf0f84119b3843763780d65e195e5e2eea924a
kandika-Rohan <rohankandika05@gmail.com>
2024-09-13 01:14:34 +0530

    Time: 39 ms (29.79%), Space: 16.3 MB (36.56%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
 
+        vector<vector<int>>mat(n,vector<int>(n,1e9));
 
-        vector<vector<int>>adj(n,vector<int>(n,1e9));
         for(int i=0;i<n;i++){
-            adj[i][i]=0;
-        }
-        for(auto it:edges){
-            int u=it[0];
-            int v=it[1];
-            int wt=it[2];
-            adj[u][v]=wt;
-            adj[v][u]=wt;
-        }
-
-        for(int via=0;via<n;via++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(adj[i][via] == 1e9 || adj[via][j] == 1e9)continue;
-                    adj[i][j]=min(adj[i][j],adj[i][via]+adj[via][j]);
+            for(int j=0;j<n;j++){
+                if(i == j){
+                mat[i][j]=0;
                 }
             }
         }
 
-        for(auto it:adj){
+        for(auto it:edges){
+            int  u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            mat[u][v]=wt;
+            mat[v][u]=wt;
+        }
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+        for(auto it:mat){
             for(auto i:it){
                 cout<<i<<" ";
             }
             cout<<endl;
         }
-
-        int city=-1;
-        int citycnt=n;
-
+        int ans=-1;
+        int cnt=INT_MAX;
         for(int i=0;i<n;i++){
-            int cnt=0;
+            int c=0;
             for(int j=0;j<n;j++){
-                if(adj[i][j] <= distanceThreshold){
-                    cnt++;
+                if(mat[i][j] <= distanceThreshold){
+                    c++;
                 }
             }
-
-            if (cnt <= citycnt) { 
-                citycnt = cnt;
-                city = i;
+            if(c<=cnt){
+                cnt=c;
+                ans=i;
             }
         }
-        return city;
+        return ans;
+
     }
 };
