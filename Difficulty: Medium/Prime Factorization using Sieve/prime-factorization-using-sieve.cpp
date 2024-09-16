4d7628158e8a0cbcadb8e396be74909e0faab160
kandika-Rohan <rohankandika05@gmail.com>
2024-09-16 22:00:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prime Factorization using Sieve/prime-factorization-using-sieve.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void sieve() {}
+
+    vector<int> findPrimeFactors(int N) {
+
+        // Write your code here
+        vector<int>ans;
+        
+        int i=2;
+        
+        while(N>1){
+            
+            if(N%i == 0){
+                ans.push_back(i);
+                N=N/i;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        int n;
+        cin >> n;
+
+        Solution obj;
+        obj.sieve();
+        vector<int> vec = obj.findPrimeFactors(n);
+        for (int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
