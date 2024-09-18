d88ac50ab875da7cad0de4cc50b96135bb07f76a
kandika-Rohan <rohankandika05@gmail.com>
2024-09-18 14:06:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
+++ Difficulty: Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
@@ -10,25 +10,35 @@ class Solution
     public:
     //Function to find the minimum number of swaps required to sort the array. 
 	int minSwaps(vector<int>&nums)
-    {
-        vector<int>ans(nums.begin(),nums.end());
-        sort(ans.begin(),ans.end());
-        unordered_map<int,int>mp;
-        for(int i=0;i<ans.size();i++) {
-            mp[ans[i]]=i;
-        }
-        int count=0;
-        for(int i=0;i<nums.size();i++) {
-            if(mp[nums[i]]!=i) {
-                swap(nums[i],nums[mp[nums[i]]]);
-                count++;
-                i--;
-            }
-        }
-        return count;
-    }
+	{
+	    // Code here
+	    unordered_map<int,int>map;
+	    
+	    vector<int>val=nums;
+	    
+	    sort(val.begin(),val.end());
+	    
+	    for(int i=0;i<nums.size();i++){
+	        
+	        map[val[i]]=i;
+	    }
+	    
+	    int cnt=0;
+	    
+	    for(int i=0;i<nums.size();i++){
+	        
+	        if(i != map[nums[i]]){
+	            
+	            int ind=map[nums[i]];
+	            swap(nums[i],nums[ind]);
+	            i--;
+	            cnt++;
+	        }
+	    }
+	    
+	    return cnt;
+	}
 };
-
 
 //{ Driver Code Starts.
 int main(){
