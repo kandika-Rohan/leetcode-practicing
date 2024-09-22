579fc7d7008bc15d0fcb9161fdbdf0cdf34e19f4
kandika-Rohan <rohankandika05@gmail.com>
2024-09-22 22:43:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+#include <iostream>
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+vector<int> take() {
+    vector<int> arr;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    int number;
+    while (ss >> number) {
+        arr.push_back(number);
+    }
+    return arr;
+}
+
+Node* inputList(int size, vector<int> v) {
+    if (size == 0)
+        return NULL;
+
+    int val = v[0];
+
+    Node* head = new Node(val);
+    Node* tail = head;
+
+    for (int i = 0; i < size - 1; i++) {
+        val = v[i + 1];
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+
+// } Driver Code Ends
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    // Function to find intersection point in Y shaped Linked Lists.
+    
+    int no_ofelements(Node*root){
+        
+        if(!root)return 0;
+        
+        return 1+no_ofelements(root->next);
+    }
+    int solve(Node*head1,Node*head2){
+        
+         Node*temp1=head1;
+         Node*temp2=head2;
+            
+            while(temp1 && temp2){
+                
+                if(temp1 == temp2){
+                    
+                    return temp1->data;
+                }
+                temp1=temp1->next;
+                temp2=temp2->next;
+                
+            }
+            
+            return -1;
+    }
+    int intersectPoint(Node* head1, Node* head2) {
+        
+        int cnt1=no_ofelements(head1);
+        int cnt2=no_ofelements(head2);
+        
+        if(cnt1 == cnt2){
+            return solve(head1,head2);
+        }
+        
+        else if(cnt1-cnt2>0){
+            
+            int diff=cnt1-cnt2;
+            
+            Node*temp=head1;
+            
+            while(temp && diff>0){
+                temp=temp->next;
+                diff--;
+            }
+            
+            return solve(temp,head2);
+        }
+        
+        
+        else{
+            int diff=cnt2-cnt1;
+            
+            Node*temp=head2;
+            
+            while(temp && diff>0){
+                temp=temp->next;
+                diff--;
+            }
+            
+            return solve(temp,head2);
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main() {
+    srand(time(0));
+    int T, n1, n2, n3;
+
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+
+        vector<int> v1 = take();
+        vector<int> v2 = take();
+        vector<int> v3 = take();
+        int n1 = v1.size(), n2 = v2.size(), n3 = v3.size();
+
+        Node* head1 = NULL;
+        Node* head2 = NULL;
+        Node* common = NULL;
+
+        head1 = inputList(n1, v1);
+        head2 = inputList(n2, v2);
+        common = inputList(n3, v3);
+
+        Node* temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+        Solution ob;
+        cout << ob.intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
