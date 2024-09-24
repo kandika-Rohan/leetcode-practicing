22c52cbcbd1af0d457ef741731202eb0a8a267a9
kandika-Rohan <rohankandika05@gmail.com>
2024-09-24 13:29:52 +0530

    Time: 229 ms (85.08%), Space: 160.8 MB (56.99%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,36 +1,64 @@
 
+class Trie{
+    private:
+    struct trie{
+        trie*digits[10];
+
+        trie(){
+        fill(begin(digits),end(digits),nullptr);
+        }
+    };
+    public:
+    trie*root;
+
+    Trie(){
+        root=new trie();
+    }
+
+    void insert(int n){
 
 
+        trie*node=root;
+        string num=to_string(n);
+
+        for(auto it:num){
+            int val=it-'0';
+            if(node->digits[val] == nullptr){
+               node->digits[val]=new trie();
+            }
+            node=node->digits[val];
+        }
+    }
+
+    int search(int n){
+
+        trie*node=root;
+        string num=to_string(n);
+        int cnt=0;
+        for(auto it:num){
+            int val=it-'0';
+            if(node->digits[val] == nullptr){
+               break;
+            }
+            cnt++;
+            node=node->digits[val];
+        }
+
+        return cnt;
+    }
+};
 class Solution {
 public:
-    class Node{
-        public:
-            Node* digits[10];   
-    };
-    void add(Node* head, int num){
-        string t = to_string(num);
-        Node* ptr = head;
-        for(auto c: t){
-            if(!ptr->digits[c - '0']) ptr->digits[c - '0'] = new Node();
-            ptr = ptr->digits[c-'0'];
-        }
-    }
-    int check(Node* head, int num){
-        Node* ptr = head;
-        string t = to_string(num);
-        int len = 0;
-        for(auto c: t){
-            if(ptr->digits[c - '0']) len++;
-            else break;
-            ptr = ptr->digits[c-'0'];
-        }
-        return len;
-    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        int ans = 0;
-        Node* head = new Node();
-        for(auto num: arr1) add(head, num);
-        for(auto num: arr2) ans = max(ans, check(head, num));
-        return ans;
+        Trie t;
+        for(auto it:arr1){
+            t.insert(it);
+        }
+        int maxi=0;
+        for(auto it:arr2){
+            maxi=max(maxi,t.search(it));
+        }
+
+        return maxi;
     }
 };
