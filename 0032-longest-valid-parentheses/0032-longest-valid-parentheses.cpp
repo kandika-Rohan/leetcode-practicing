82efdee7bd648fa86a0bb5bc43bcd58ca8d9ee71
kandika-Rohan <rohankandika05@gmail.com>
2024-09-26 11:57:06 +0530

    Time: 3 ms (74.23%), Space: 9.2 MB (37.4%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        stack<int>st;
+
+        st.push(-1);
+        int maxi=0;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] =='('){
+                st.push(i);
+            }
+            else{
+
+                st.pop();
+
+                if(st.empty()){
+                    st.push(i);
+                }
+                maxi=max(maxi,i-st.top());
+            }
+        }
+        return maxi;
+    }
+};
