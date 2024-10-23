31605f5ddf40f485745d5903f1a356a28f464875
kandika-Rohan <rohankandika05@gmail.com>
2024-10-24 00:11:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Modify the Array/modify-the-array.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) {
+        // Complete the function
+        
+        
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            if(arr[i] == arr[i+1]){
+                arr[i]=arr[i]+arr[i];
+                arr[i+1]=0;
+            }
+        }
+        vector<int>ans(n,0);
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i] >0){
+                ans[j]=arr[i];
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.modifyAndRearrangeArray(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
