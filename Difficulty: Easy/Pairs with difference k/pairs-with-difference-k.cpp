cc68c644d043b25d2c450390a90370ecb1f1cc13
kandika-Rohan <rohankandika05@gmail.com>
2024-10-30 00:22:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with difference k/pairs-with-difference-k.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        
+        int cnt=0;
+        
+        unordered_map<int,int>map;
+        
+        for(auto it:arr){
+            map[it]++;
+        }
+        
+        map[cnt]=1;
+        
+        for(int i=0;i<arr.size();i++){
+            
+            int diff=arr[i]+k;
+            
+            if(map.find(diff) != map.end()){
+                cnt+=map[diff];
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
