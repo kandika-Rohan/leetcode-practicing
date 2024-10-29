f17939e3deb2f705e45bc329e2c616df6bcb25e7
kandika-Rohan <rohankandika05@gmail.com>
2024-10-29 23:35:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count of Subarrays/count-of-subarrays.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+
+	ll countSubarray(int arr[], int n, int k) {
+	    // code here
+	    
+	    long sum=0,res=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>k){
+                sum=i+1;
+            }
+            res+=sum;
+        }
+        return res;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countSubarray(arr, n, k);
+        cout << ans << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
