373d4394d6587e8355ee68b2fe3b5be1ddf9ac34
kandika-Rohan <rohankandika05@gmail.com>
2024-12-14 11:27:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -6,34 +6,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int search(vector<int>& arr, int key) {
+    int search(vector<int>& arr, int k) {
         // complete the function here
-       int l=0;
-       int r=arr.size()-1;
-       while(l<=r){
-           int mid=l+(r-l)/2;
-           if(arr[mid]==key){
-               return mid;
-           }
-            if (arr[l] <= arr[mid]) {
-            // Check if the key is in the left half
-            if (arr[l] <= key && key < arr[mid]) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        
+        int ans=-1;
+        
+        int l=0;
+        int r=arr.size()-1;
+        
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            
+            if(arr[mid] == k){
+                return mid;
+            }
+            
+            if(arr[l]<=arr[mid]){
+                if(arr[l]<=k && arr[mid]>k){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(arr[mid]<k  && k<=arr[r]){
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
             }
         }
-        // If the right half is sorted
-        else {
-            // Check if the key is in the right half
-            if (arr[mid] < key && key <= arr[r]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-       }
-       return -1;
+        return -1;
     }
 };
 
@@ -55,6 +59,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
