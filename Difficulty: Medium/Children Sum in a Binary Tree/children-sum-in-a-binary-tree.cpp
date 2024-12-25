25b7c6f3d4ea418e934c38ff87428ffe4956c10d
kandika-Rohan <rohankandika05@gmail.com>
2024-12-26 00:20:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){80
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    int isSumProperty(Node *root)
+    {
+        if(root==nullptr){
+            return 1;
+        }
+     // Add your code here
+     queue<Node*>q;
+     q.push(root);
+     while(!q.empty()){
+         auto node=q.front();
+         q.pop();
+      int leftData = (node->left != nullptr) ? node->left->data : 0;
+        int rightData = (node->right != nullptr) ? node->right->data : 0;
+        
+       
+        if (node->left != nullptr || node->right != nullptr) {
+            if (node->data != leftData + rightData) {
+                return 0;
+            }
+        }
+             if(node->left){
+                 q.push(node->left);
+             }
+             if(node->right){
+                 q.push(node->right);
+             }
+         }
+     
+     return 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.isSumProperty(root) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
