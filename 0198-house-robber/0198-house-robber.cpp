49b141cd7f5912d5c15e89c2b274fa6f2af32990
kandika-Rohan <rohankandika05@gmail.com>
2025-01-04 00:39:43 +0530

    Time: 0 ms (100%), Space: 10.6 MB (39.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    int function(int n,vector<int>&nums,vector<int>&dp){
+
+        if(n==0){
+            return nums[n];
+        }
+
+        if(n<0){
+            return 0;
+
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int first=nums[n]+function(n-2,nums,dp);
+
+        int second=0+function(n-1,nums,dp);
+
+        return dp[n]=max(first,second);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int prev1=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1){
-                take+=prev2;
-            }
-            int nontake=prev1+0;
-            int curr=max(take,nontake);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+
+        vector<int>dp(n+1,-1);
+
+        return function(n-1,nums,dp);
+        
     }
 };
