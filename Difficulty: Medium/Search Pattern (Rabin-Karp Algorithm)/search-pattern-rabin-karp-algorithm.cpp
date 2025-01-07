26815cd4f0bf51a88fbfba28ab310ac4e8d37263
kandika-Rohan <rohankandika05@gmail.com>
2025-01-07 22:36:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -7,24 +7,72 @@ using namespace std;
 class Solution
 {
     public:
+        vector<int>kmp(string p){
+            
+            int n=p.size();
+            
+            vector<int>lps(n,0);
+            
+            int i=1;
+            int len=0;
+            
+            while(i<n){
+                
+                if(p[i] == p[len]){
+                    
+                    len++;
+                    lps[i]=len;
+                    i++;
+                }
+                else{
+                    
+                    if(len != 0){
+                        len=lps[len-1];
+                    }
+                    else{
+                        lps[i]=0;
+                        i++;
+                    }
+                }
+            }
+            
+            return lps;
+        }
         vector <int> search(string pattern, string text)
         {
-          
-          int n= text.size();
-          
-          int k= pattern.size();
-          
-          vector<int> res;
-          
-          for(int i=0; i<n; i++){
-              
-              if(pattern== text.substr(i,k)){
-                  res.push_back(i+1);
-              }
-          }
-          
-          return res;
+            vector<int>lps=kmp(pattern);
+            
+            int i=0;
+            int j=0;
+            int n=text.size();
+            
+            vector<int>ans;
+            
+            while(i<n){
+                
+                if(pattern[j] == text[i]){
+                    i++;
+                    j++;
+                }
+                
+                if(j == pattern.size()){
+                    ans.push_back(i-j+1);
+                    j=lps[j-1];
+                }
+                else if(i<n && pattern[j] != text[i]){
+                    
+                    if(j != 0){
+                        j=lps[j-1];
+                    }
+                    else{
+                        i++;
+                    }
+                }
+            }
+            
+            return ans;
         }
+     
 };
 
 //{ Driver Code Starts.
@@ -40,7 +88,9 @@ int main()
         vector <int> res = ob.search(pat, S);
         for (int i : res) cout << i << " ";
         cout << endl;
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 
