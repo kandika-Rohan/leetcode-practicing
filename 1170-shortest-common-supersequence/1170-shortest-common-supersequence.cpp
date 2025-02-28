c0a9057af527f19da423948014ee558845487e30
kandika-Rohan <rohankandika05@gmail.com>
2025-03-01 00:53:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,52 +1,80 @@
 class Solution {
 public:
-
     string shortestCommonSupersequence(string str1, string str2) {
         
 
         int n=str1.size();
+
         int m=str2.size();
+
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
+        int maxi=0;
 
         for(int i=1;i<=n;i++){
+
             for(int j=1;j<=m;j++){
+
                 if(str1[i-1] == str2[j-1]){
+
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
+
+                maxi=max(maxi,dp[i][j]);
             }
         }
 
-        string ans="";
+        
+        cout<<maxi<<endl;
+
+        string res="";
+
         int i=n;
+
         int j=m;
+
         while(i>0 && j>0){
+
             if(str1[i-1] == str2[j-1]){
-                ans+=str1[i-1];
+
+                res+=str1[i-1];
+
                 i--;
                 j--;
+
             }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                ans+=str1[i-1];
-                i--;
+            else if(dp[i][j-1]>dp[i-1][j]){
+
+
+                res+=str2[j-1];
+
+                j--;
+
             }
+
             else{
-                ans+=str2[j-1];
-                j--;
+
+                res+=str1[i-1];
+
+                i--;
             }
         }
+
         while(i>0){
-            ans+=str1[i-1];
+
+            res+=str1[i-1];
             i--;
         }
         while(j>0){
-            ans+=str2[j-1];
+
+            res+=str2[j-1];
             j--;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+
+        return res;
     }
 };
