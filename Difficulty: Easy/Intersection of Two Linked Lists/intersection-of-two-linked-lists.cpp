985e19ef0fe91fe9118f596e9c5ab4a11234b33c
kandika-Rohan <rohankandika05@gmail.com>
2025-03-13 23:47:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Linked Lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x)
+        : data(x)
+        , next(nullptr) {}
+};
+
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+    cout << '\n';
+}
+
+
+// } Driver Code Ends
+
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        unordered_map<int, int> freqMap; 
+        Node* temp = head2;
+
+       
+        while (temp) {
+            freqMap[temp->data]++;  
+            temp = temp->next;
+        }
+
+
+        Node* dummy = new Node(0);  
+        Node* tail = dummy;  
+
+        Node* temp2 = head1;
+        
+        while (temp2) {
+            if (freqMap[temp2->data] > 0) { 
+                tail->next = new Node(temp2->data);  
+                tail = tail->next;
+                freqMap[temp2->data]--;  
+            }
+            temp2 = temp2->next;
+        }
+
+        return dummy->next; 
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+// Driver program to test the above functions
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+
+    while (T--) {
+        Node *head1 = nullptr, *tail1 = nullptr;
+        Node *head2 = nullptr, *tail2 = nullptr;
+        int tmp;
+        string input1, input2;
+
+        getline(cin, input1);
+        stringstream ss1(input1);
+        while (ss1 >> tmp) {
+            Node* new_node = new Node(tmp);
+            if (!head1) {
+                head1 = new_node;
+                tail1 = new_node;
+            } else {
+                tail1->next = new_node;
+                tail1 = new_node;
+            }
+        }
+
+        getline(cin, input2);
+        stringstream ss2(input2);
+        while (ss2 >> tmp) {
+            Node* new_node = new Node(tmp);
+            if (!head2) {
+                head2 = new_node;
+                tail2 = new_node;
+            } else {
+                tail2->next = new_node;
+                tail2 = new_node;
+            }
+        }
+
+        Solution obj;
+        Node* result = obj.findIntersection(head1, head2);
+        printList(result);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
