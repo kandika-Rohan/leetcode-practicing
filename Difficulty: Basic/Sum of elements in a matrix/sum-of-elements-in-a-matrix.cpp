0c278c4db1f52696f477c523e07f793e6b0d92d5
kandika-Rohan <rohankandika05@gmail.com>
2025-03-19 22:44:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of elements in a matrix/sum-of-elements-in-a-matrix.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int sumOfMatrix(int N, int M, vector<vector<int>> Grid) {
+        // code here
+        int sum=0;
+        for(auto it:Grid){
+            for(auto i:it){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> Grid(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) cin >> Grid[i][j];
+        Solution ob;
+        cout << ob.sumOfMatrix(N, M, Grid) << "\n";
+    
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
